9a233f6d6c32edb24c49ec77ee46e57034721325
Adam Magaluk <AdamMagaluk@gmail.com>
2013-02-18 16:24:49 -0500

    Now writes address and parcel id to csv and entire output to json file

--- scrape-accesskent.js
+++ scrape-accesskent.js
@@ -1,16 +1,11 @@
-// Scraping Made Easy with jQuery and SelectorGadget 
-// (http://blog.dtrejo.com/scraping-made-easy-with-jquery-and-selectorga)
-// by David Trejo
-// 
-// Install node.js and npm:
-//    http://joyeur.com/2010/12/10/installing-node-and-npm/
-// Then run
-//    npm install jsdom jquery http-agent
-//    node numresults.js
-// 
 var util = require('util')
   , request = require('request')
+  , async = require('async')
+  , jsdom = require('jsdom').jsdom
+  , S = require('string')
+  , fs = require('fs');
 
+var config = require('./config/config.json');
 
 function paddy(n, p, c) {
     var pad_char = typeof c !== 'undefined' ? c : '0';
@@ -18,15 +13,14 @@ function paddy(n, p, c) {
     return (pad + n).slice(-pad.length);
 }
 
-
 var ParcelPin = function(id){
   var self = this;
   this.id = {
     county : 0
-    ,township: 0
-    ,section: 0
-    ,chunk: 0
-    ,id: 0
+    , township: 0
+    , section: 0
+    , chunk: 0
+    , id: 0
   };
 
   var a = id;
@@ -41,8 +35,8 @@ var ParcelPin = function(id){
   Object.keys(self.id).forEach(function(k){
     self.id[k] = Number(a[i]);
     i++;
-  })
-}
+  });
+};
 ParcelPin.prototype.toString = function() {
   return this.id.county + "-" + this.id.township + "-" + this.id.section + "-" + this.id.chunk + "-" + paddy(this.id.id,3);
 };
@@ -52,48 +46,162 @@ ParcelPin.prototype.searchFormData = function() {
     parcelNo2 : ""+self.id.township,
     parcelNo3 : ""+self.id.section,
     parcelNo4 : ""+self.id.chunk,
-    parcelNo5 : paddy(self.id.id,3),
-  }
+    parcelNo5 : paddy(self.id.id,3)
+  };
 };
 
-var parcel = new ParcelPin("41-14-29-181-007");
 
-var yearsToCheck = [2010,2011,2012];
+var output = {
+  totalChecked : 0,
+  totalFinished : 0,
+  matched : [],
+  maxId : 0
+};
 
-for(var i=0;i<50;i++){
-  checkProp(new ParcelPin([41,14,29,181,i]));
+var startTime = new Date().getTime();
+var endTime;
+var last = 0;
+function printPercentage(){
+  var per = Math.round(100.0*(output.totalFinished / output.totalChecked));
+  
+  if(per % 1 == 0 && per != last){
+   console.log(per + "% complete " + output.totalFinished + "/"+output.totalChecked);
+   last = per;
+  }
+}
+
+function consoleShow(){
+  var time =  endTime-startTime;
+  console.log("all items have been processed " +time + "/"+output.totalChecked+" Avg:" + (time/output.totalChecked) + ' Found:' + output.totalMatched + " MaxId:" + output.maxId );
+  output.matched.forEach(function(item){
+    console.log("   "+item.parcel.toString()+" "+item.info.addressSteet + " " + item.info.area);
+    item.years.forEach(function(y){
+      console.log("    - " + y.year + " - Tax Value:" + y.taxValue + "   Delinquent Tax Amount Due:"+y.taxMissing)
+    });
+  });
+}
+
+var q = async.queue(function (task, callback) {
+    checkProp(task.parcel,function(err,ret){
+      if(err) return console.error(err);
+      if(ret.foundInSearch){
+        if(task.parcel.id.id > output.maxId){
+          output.maxId = task.parcel.id.id;
+        }
+      }
+      if(ret.years.length == config.yearsToCheck.length){
+        output.matched.push(ret);
+      }
+      output.totalFinished++;
+      printPercentage();
+      callback();
+    });
+},config.workers);
+
+
+function writeToFile(){
+  var stream = fs.createWriteStream(config.outFile);
+  stream.once('open', function(fd) {
+      output.matched.forEach(function(item){
+        stream.write(item.parcel.toString()+","+item.info.addressSteet+" "+item.info.area+"\n");
+      })
+  });
+
+  fs.writeFile(config.outFile+".json",JSON.stringify(output), function(err) {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log("The file was saved!");
+      }
+  }); 
+
+}
+
+q.drain = function() {
+  endTime = new Date().getTime();
+  output.totalMatched = output.matched.length; 
+  consoleShow();
+  writeToFile();
+};
+
+for(var iCounty=config.range.county[0];iCounty<=config.range.county[1];iCounty++){
+  for(var iTownship=config.range.township[0];iTownship<=config.range.township[1];iTownship++){
+    for(var iSection=config.range.section[0];iSection<=config.range.section[1];iSection++){
+      for(var iChunk=config.range.chunk[0];iChunk<=config.range.chunk[1];iChunk++){
+        for(var iId=config.range.id[0];iId<=config.range.id[1];iId++){
+          output.totalChecked++;
+          q.push({parcel: new ParcelPin([iCounty,iTownship,iSection,iChunk,iId])});
+        }
+      }
+    }
+  }
 }
 
 function checkProp(parcel,callback){
+  var ret = {
+    parcel : parcel,
+    foundInSearch : false,
+    bodyEmpty : true,
+    years : []
+  };
 
-var jar = request.jar();
-request.get('https://www.accesskent.com/Property/search.do',{jar : jar},function(e,res,body){
-  if(e) return console.error(e)
-  request.post('https://www.accesskent.com/Property/search.do',{form: parcel.searchFormData(),jar : jar },function(e,res,body){
-      if(e) return console.error(e)
+  var jar = request.jar();
+  request.get(config.baseUrl+'/search.do',{jar : jar},function(e,res,body){
+    if(e) return callback(e);
+    request.post(config.baseUrl+'/search.do',{form: parcel.searchFormData(),jar : jar },function(e,res,body){
+        if(e) return callback(e);
 
-      if(body.search('There were no result found.') != -1){
-        console.log(parcel.toString() + " not in search");      
-        return;
-      }
-       
-      request.get('https://www.accesskent.com/Property/realEstate.do?parcelNo='+parcel.toString()+'&ele=0',{jar : jar},function(e,res,body){
-        if(e) return console.error(e)
-        request.get('https://www.accesskent.com/Property/delqSearch.do',{jar : jar},function(e,res,body){
-          if(e) return console.error(e)
-            if(body){
-              yearsToCheck.forEach(function(y){
-                if(body.search('parclno='+parcel.toString()+'&taxYr='+y) != -1){
-                  console.log(parcel.toString() + " has back taxes for year " + y);      
-                }else{
-                  console.log(parcel.toString() + " has no back taxes for year " + y);      
-                }
-              })
-            }else{
-              console.log(parcel.toString() + " missing body");      
-            }
-        })
-      })
-    })
-});
+        if(body.search('There were no result found.') != -1){
+          return callback(null,ret);
+        }
+        ret.foundInSearch = true;
+
+        request.get(config.baseUrl+'/realEstate.do?parcelNo='+parcel.toString()+'&ele=0',{jar : jar},function(e,res,body){
+          if(e) return callback(e);
+
+          ret.info = getParcelInformation(body) || {};
+
+          request.get(config.baseUrl+'/delqSearch.do',{jar : jar},function(e,res,body){
+            if(e) return callback(e);
+              if(body){
+                ret.bodyEmpty = false;
+                config.yearsToCheck.forEach(function(y){
+                  var s = body.search('parclno='+parcel.toString()+'&taxYr='+y);
+                  if(s != -1){
+
+                    var x = S(body.substr(s,550)).stripTags().s.replace('\t','').split("\n");
+                    var re = [];
+                    x.forEach(function(test){
+                      if(S(test).trim().s != ""){ re.push(S(test).trim().s); }
+                    });
+
+                    ret.years.push({
+                      year : y,
+                      taxValue : re[1],
+                      taxMissing : re[6].substr(1,re[6].length)
+                    });
+                  }
+                });
+              }
+              return callback(null,ret);
+          });
+        });
+      });
+  });
+}
+
+function getParcelInformation(body){
+  var window = jsdom(body).createWindow()
+    , $ = require('jquery').create(window);
+    var ret = {};
+    var find = [
+      {id : 4,name : "area"},
+      {id : 6,name : "status"},
+      {id : 7,name : "addressSteet"},
+    ];
+    var form = $("form");
+    find.forEach(function(f){
+      ret[f.name] = S($('.avtable-inner tr:nth-child('+f.id+') td:nth-child(2)',form).text()).trim().s;
+    });
+    return ret;
 }
